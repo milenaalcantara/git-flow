2d72e6076571a92afc4a39820c500d52f17bedc4
Milena Alcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2022-06-03 10:52:26 -0300

    created initial app

---
+++ confy-food/confy-food/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  confy-food
+//
+//  Created by Milena Lima de Alcântara on 03/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
