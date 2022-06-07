79f614bf596e7f73a267b049144c64d367be0cf9
Joan Wilson <joanwmoliveira@gmail.com>
2022-06-07 10:53:33 -0300

    delete main storyboard

--- confy-food/confy-food/ViewController.swift
+++ confy-food/confy-food/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var nomString = "Joan"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
