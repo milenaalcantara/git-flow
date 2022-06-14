5109f5f30b07da089304cddd7d6c9f921919c39f
Milena Alcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2022-06-14 09:10:00 -0300

    create NotFound screen

---
+++ confy-food/confy-food/Controllers/NotFoundViewController.swift
@@ -0,0 +1,24 @@
+//
+//  NotFoundViewController.swift
+//  confy-food
+//
+//  Created by Milena Lima de Alcântara on 10/06/22.
+//
+
+import UIKit
+
+class NotFoundViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
